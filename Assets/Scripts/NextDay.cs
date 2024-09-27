e7b5e7dad764becd62260747b35226f001d89c54
Jiri Gebauer <jiri.gebauer.jr@gmail.com>
2024-09-27 23:10:14 +0200

    First version of DayEnd

---
+++ Assets/Scripts/NextDay.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextDay : MonoBehaviour
+{
+    public void finish()
+    {
+        SceneManager.LoadScene("Main");
+        // to do
+    }
+}
