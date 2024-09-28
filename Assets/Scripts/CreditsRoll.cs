54c9132b6d78d8679db73f7f14303fe96a1ddd45
Jiri Gebauer <jiri.gebauer.jr@gmail.com>
2024-09-28 10:16:37 +0200

    Credits

---
+++ Assets/Scripts/CreditsRoll.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsRoll : MonoBehaviour
+{
+    public float speed = 50f; // Speed of the credit roll
+    public RectTransform creditsTransform; // Reference to the RectTransform of the credits
+
+    void Update()
+    {
+        // Move the credits upward by modifying its anchoredPosition
+        creditsTransform.anchoredPosition += new Vector2(0, speed * Time.deltaTime);
+        if (creditsTransform.anchoredPosition.y > 1000f) Application.Quit();
+    }
+}
